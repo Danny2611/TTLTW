94f54a6b87ab45f3d655e50d250eebf7d5779b38
OzuSus <21130327@st.hcmuaf.edu.vn>
2025-04-16 11:16:50 +0700

    seperate doPost and doGet, using sweetalert to show success popup

--- src/main/java/com/example/finallaptrinhweb/controller/admin/ViewOrder.java
+++ src/main/java/com/example/finallaptrinhweb/controller/admin/ViewOrder.java
@@ -14,11 +14,25 @@ import java.util.List;
 
 import static com.example.finallaptrinhweb.dao.OrderDAO.*;
 import static com.example.finallaptrinhweb.dao.OrderProductDAO.loadOrderProductByOrderId;
-
 @WebServlet("/admin/view-order")
 public class ViewOrder extends HttpServlet {
+
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        doGet(request, response);
+        int id = Integer.parseInt(request.getParameter("id"));
+        String status = request.getParameter("status");
+
+        ProductDAO productDAO = new ProductDAO();
+
+        if ("Giao hàng thành công".equals(status)) {
+            List<OrderProduct> productList = loadOrderProductByOrderId(id);
+            for (OrderProduct op : productList) {
+                productDAO.updateStockProduct(op.getProductId(), op.getQuantity());
+            }
+        }
+
+        updateStatusById(id, status);
+
+        response.sendRedirect("total-report?update_success=true");
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -26,32 +40,15 @@ public class ViewOrder extends HttpServlet {
         request.setAttribute("title", "Chi tiết đơn hàng");
 
         int id = Integer.parseInt(request.getParameter("id"));
-        String status = request.getParameter("status");
-        ProductDAO productDAO = new ProductDAO();
-        if (status != null) {
-            // Nếu là "Giao hàng thành công", cập nhật tồn kho
-            if (status.equals("Giao hàng thành công")) {
-                List<OrderProduct> productList = loadOrderProductByOrderId(id);
-                for (OrderProduct op : productList) {
-                   productDAO.updateStockProduct(op.getProductId(), op.getQuantity());
-                }
-            }
-
-            // Cập nhật trạng thái đơn hàng
-            updateStatusById(id, status);
-        }
-
         Order order = loadOrder_view(id);
         request.setAttribute("view_order", order);
-        request.setAttribute("t_p",order.getTotalPay());
-        request.setAttribute("ship",order.getShipPrice());
-        double total = order.getTotalPay()+order.getShipPrice();
-        request.setAttribute("total",total);
+        request.setAttribute("t_p", order.getTotalPay());
+        request.setAttribute("ship", order.getShipPrice());
+        double total = order.getTotalPay() + order.getShipPrice();
+        request.setAttribute("total", total);
         List<OrderProduct> productList = loadOrderProductByOrderId(id);
-
         request.setAttribute("list_pro", productList);
 
         request.getRequestDispatcher("view-order.jsp").forward(request, response);
-
     }
 }
